91576eb7bffa81083cde01b12cc023bc61ff46dd
Hussein Elbaroudy <hkasem@gmail.com>
2014-07-18 18:29:00 +0200

    Bonus problem: Guess The number

---
+++ ruby/day1/GuessTheNumber.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+random_num = rand(1..10)
+puts "Guess the number between 0 and 10"
+user_num = gets.to_i
+
+times = 1
+while user_num != random_num 
+	print "Too Low" if user_num < random_num
+	print "Too Hight" if user_num > random_num
+	times = times+1
+	puts ", Try again!!"
+	user_num = gets.to_i
+end
+puts "You win after #{times} tiemes"
